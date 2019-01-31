fdea37ee955463a3662747f788d62d3c724ccc8a
Max Musatov <m1kc@yandex.ru>
2019-01-31 15:51:04 +0300

    Create date.py

---
+++ date.py
@@ -0,0 +1,30 @@
+import arrow
+
+
+def parse_month(yyyy_mm):
+	a = arrow.get(yyyy_mm, 'YYYY-M')
+	return (a.year, a.month)
+
+
+def ensure_iterable(x):
+	if isinstance(x, list): return x
+	if isinstance(x, tuple): return x
+	# TODO other cases
+	return (x,)
+
+def parse_date(s, format_or_formats):
+	d = None
+	for f in ensure_iterable(format_or_formats):
+		try:
+			d = arrow.get(s, f)
+			break
+		except:
+			pass
+	if d == None:
+		raise ValueError(f"Could not parse date `{s}` with any of the specified formats")
+	return d
+
+def normalize_date(s, input_formats, output_format, allow_empty=True):
+	if s == None and allow_empty:
+		return None
+	return parse_date(s, input_formats).format(output_format)
