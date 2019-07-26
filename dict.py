da298943c104929d5d9d1925a1216dea9b8cc78e
m1kc (Max Musatov) <m1kc@yandex.ru>
2019-07-26 11:54:09 +0300

    django/dict: add docs

--- dict.py
+++ dict.py
@@ -1,14 +1,37 @@
 def find_field(d, candidates):
+	"""
+	Given a dict and a list of possible keys, find the first key
+	which is included into this dict. Throws ValueError if not found.
+	"""
 	for c in candidates:
 		if c in d:
 			return c
 	raise ValueError(f"Can't find any of: {candidates}")
 
 def find_value(d, candidates):
+	"""
+	Given a dict and a list of possible keys, find the first key
+	which is included into this dict and return its value.
+	Throws ValueError if not found.
+	"""
 	fieldname = find_field(d, candidates)
 	return d[fieldname]
 
 def normalize_dict(source, replacements, allow_original_key=True):
+	"""
+	Given a dict and a map: reference key -> [] possible replacements,
+	return a new dict where all keys are guaranteed to be reference keys
+	with values taken from any of replacement keys.
+
+	Typically used to normalize a dataset where the same info can be listed
+	under different names.
+
+	Example map:
+	{
+		"artist": ["Artist", "ARTIST"],
+		"year": ["Year", "yr", "yob"],
+	}
+	"""
 	ret = {}
 	for key in replacements:
 		candidates = replacements[key]
@@ -17,3 +40,4 @@ def normalize_dict(source, replacements, allow_original_key=True):
 		value = find_value(source, candidates)
 		ret[key] = value
 	return ret
+
