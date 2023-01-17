f22c2e5a18401f00456fa0cdc3b2a829d567a341
Kirill_Lekhov <k.lekhov@comearth.ru>
2023-01-17 09:37:28 +0300

    Refactor array module

--- notalib/array.py
+++ notalib/array.py
@@ -1,14 +1,25 @@
-def as_chunks(arr, n):
+from typing import Iterable, Any, Sequence, Generator
+
+
+def as_chunks(seq: Sequence, n: int) -> Generator:
 	"""
-	Yield successive n-sized chunks from arr.
+	Yield successive n-sized chunks from sequence.
 	"""
 	assert n >= 1
-	for i in range(0, len(arr), n):
-		yield arr[i:i + n]
+
+	for i in range(0, len(seq), n):
+		yield seq[i:i + n]
 
 
-def ensure_iterable(x):
-	if isinstance(x, list): return x
-	if isinstance(x, tuple): return x
-	# TODO other cases
+def ensure_iterable(x: Any) -> Iterable:
+	"""
+	Keeps iterable things like lists intact, turns single values into single-element lists. Useful for functions that
+	can accept both.
+
+	TODO:
+		Add other cases
+	"""
+	if isinstance(x, list) or isinstance(x, tuple):
+		return x
+
 	return (x,)
--- notalib/array_test.py
+++ notalib/array_test.py
@@ -1,64 +1,72 @@
 from .array import as_chunks, ensure_iterable
 
+from functools import reduce
+from operator import add
+
+import pytest
 from hypothesis import given
 from hypothesis.strategies import lists, integers
 
 
 def _collect(x):
-	ret = []
-	for i in x: ret.append(i)
-	return ret
+	return list(x)
 
 
 def _flatten(arr):
-	ret = []
-	for subarr in arr:
-		for elem in subarr:
-			ret.append(elem)
-	return ret
+	return reduce(add, arr, [])
 
 
-def test_as_chunks():
-	assert _collect(as_chunks([], 5)) == []
-	assert _collect(as_chunks([1,2,3], 5)) == [[1,2,3]]
-	assert _collect(as_chunks([1,2,3], 2)) == [[1,2],[3]]
-	assert _collect(as_chunks([1,2,3], 1)) == [[1],[2],[3]]
+@pytest.mark.parametrize(
+	"sequence, chunk_size, expected_result",
+	[
+		([], 5, []),
+		([1, 2, 3], 5, [[1, 2, 3]]),
+		([1, 2, 3], 2, [[1, 2], [3]]),
+		([1, 2, 3], 1, [[1], [2], [3]]),
+	]
+)
+def test_as_chunks(sequence, chunk_size, expected_result):
+	assert _collect(as_chunks(sequence, chunk_size)) == expected_result
 
 
 @given(lists(integers()), integers(min_value=1))
 def test_as_chunks_correctness(source, x):
-	result = _collect(as_chunks(source, x))
-	num_short = 0
-	last_short = False
-	for e in result:
-		assert len(e) <= x
-		if len(e) < x:
-			num_short += 1
-			last_short = True
-	assert num_short <= 1
-	if num_short > 0:
-		assert last_short
-	assert(_flatten(result) == source)
+	_test_as_chunks(source, x)
 
 
 @given(lists(integers(), max_size=10), integers(min_value=1, max_value=20))
 def test_as_chunks_smallscale(source, x):
+	_test_as_chunks(source, x)
+
+
+def _test_as_chunks(source, x):
 	result = _collect(as_chunks(source, x))
 	num_short = 0
 	last_short = False
+
 	for e in result:
 		assert len(e) <= x
+
 		if len(e) < x:
 			num_short += 1
 			last_short = True
+
 	assert num_short <= 1
+
 	if num_short > 0:
 		assert last_short
-	assert(_flatten(result) == source)
+
+	assert _flatten(result) == source
 
 
-def test_ensure_iterable():
-	assert ensure_iterable(1) == (1,)
-	assert ensure_iterable('x') == ('x',)
-	assert ensure_iterable(['x']) == ['x']
-	assert ensure_iterable(('x', 'y')) == ('x', 'y')
+@pytest.mark.parametrize(
+	"src, exp",
+	[
+		(1, (1,)),
+		('x', ('x',)),
+		(['x'], ['x']),
+		(('x', 'y'), ('x', 'y')),
+	]
+)
+def test_ensure_iterable(src, exp):
+	assert ensure_iterable(src) == exp
